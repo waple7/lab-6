c8fbd9c27b8ffffe587bc20a2b9041364abc1b7c
waple7 <91288621+waple7@users.noreply.github.com>
2021-10-21 21:45:21 +0300

    Add files via upload

---
+++ lab 6 number 1 and 2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+    char *arr=NULL;
+    arr=malloc(sizeof(char)*4);
+    *arr='W';
+    *(arr+1)='O';
+    *(arr+2)='R';
+    *(arr+3)='K';
+    printf("array value:\t");
+    printf("%c\t",arr[0]);
+    printf("%c\t",arr[1]);
+    printf("%c\t",arr[2]);
+    printf("%c\t",arr[3]);
+    free(arr);
+    arr=NULL;
+
+    return 0;
+
+}
