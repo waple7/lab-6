737101ed36f30824d4a1182d9bfbcf69c8fb4b11
waple7 <91288621+waple7@users.noreply.github.com>
2021-10-22 09:02:34 +0300

    Update lab 6 number 1 and 2.c

--- lab 6 number 1 and 2.c
+++ lab 6 number 1 and 2.c
@@ -2,19 +2,22 @@
 #include<stdlib.h>
 
 int main() {
-    char *arr=NULL;
-    arr=malloc(sizeof(char)*4);
-    *arr='W';
-    *(arr+1)='O';
-    *(arr+2)='R';
-    *(arr+3)='K';
+    char *arr = NULL;
+    arr = malloc(sizeof(char) * 4);
+    
+    *arr = 'W';
+    *(arr + 1) = 'O';
+    *(arr + 2) = 'R';
+    *(arr + 3) = 'K';
+    
     printf("array value:\t");
-    printf("%c\t",arr[0]);
-    printf("%c\t",arr[1]);
-    printf("%c\t",arr[2]);
-    printf("%c\t",arr[3]);
+    printf("%c\t", arr[0]);
+    printf("%c\t", arr[1]);
+    printf("%c\t", arr[2]);
+    printf("%c\t", arr[3]);
+    
     free(arr);
-    arr=NULL;
+    arr = NULL;
 
     return 0;
 
